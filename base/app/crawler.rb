dfe7e4dfe43e894759f90adcb145dbcd737c2bbc
mhkohei <garula.oze498@gmail.com>
2015-03-08 00:47:41 +0900

    fix crawler

--- base/app/crawler.rb
+++ base/app/crawler.rb
@@ -7,17 +7,18 @@ require 'twitter'
 require 'tweetstream'
 require 'json'
 require 'fileutils'
-require 'MeCab'
+
+
+
 
 
 TweetStream.configure do |config|
-  config.consumer_key = '********',
-  config.consumer_secret = '********',
-  config.oauth_token = '********',
-  config.oauth_token_secret = '********',
+  config.consumer_key = 'BGTuRGTfFnUUxSarSswhA',
+  config.consumer_secret = '91sjht13HbtMNf8RJvyX7ia00Z9LmOsMxEF7o8wSC0',
+  config.oauth_token = '600384789-pv9ORfn1v3NT182o6JGwVVXfuC9Cu0mW37amGoT7',
+  config.oauth_token_secret = 'XKxNJYx2xyjfPX2BBCshWV0UQGBxO0itmn8zNEzXWxHFP',
   config.auth_method = :oauth
 end
-
 
 
 client = TweetStream::Client.new
@@ -31,8 +32,8 @@ t_map = {}
 @statuses = []
 client.sample do |status|
   if status.user.lang == "ja" && !status.text.index("RT") && !status.text.index("http:") && !status.text.index("@") && !status.text.index("https:") && !status.text.index("jp") && !status.text.index("com")
-     nm.parse(status[5]) do |n|
-          t_map[n.surface] = t_map[n.surface] ? t_map[n.surface] + 1 : 1
+     nm.parse(status.text) do |n|
+       t_map[n.surface] = t_map[n.surface] ? t_map[n.surface] + 1 : 1
           @statuses << status
            client.stop if @statuses.size >= 100
         end
@@ -40,10 +41,10 @@ client.sample do |status|
    end
 
 t_map = t_map.sort_by {|k, v| v}
+@arrayNum = []
 
-File.open('latest.csv', 'w'){|f|
+File.open('latest.json', 'w'){|f|
   t_map.each do |word, count|
-   puts rank
-   f.write "#{word}, #{count}\n"
+   f.write "#{word} #{count}\n"
  end
  }
